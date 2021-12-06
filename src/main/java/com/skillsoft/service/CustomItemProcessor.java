7420181661a211e751752d1266110fc09b8031eb
Gabriel <gvscceca@gmail.com>
2021-12-06 09:41:42 -0300

    all application finish

---
+++ src/main/java/com/skillsoft/service/CustomItemProcessor.java
@@ -0,0 +1,17 @@
+
+package com.skillsoft.service;
+
+import com.skillsoft.model.FlightTicket;
+import org.springframework.batch.item.ItemProcessor;
+
+public class CustomItemProcessor implements ItemProcessor<FlightTicket, FlightTicket> {
+
+    public FlightTicket process(FlightTicket item){
+
+        System.out.printf("Processing %s...%n", item);
+        item.setRoute(item.getRoute().toUpperCase());
+        item.setName(item.getName().toUpperCase());
+        
+        return item;
+    }
+}
